4b3935649900012b9951fc50f4d3dfc1922231a7
Nikhil Patel <90297144+niikhilpatel@users.noreply.github.com>
2022-08-12 23:12:54 +0530

    update main and index file

--- main.js
+++ main.js
@@ -3,3 +3,23 @@
 window.addEventListener('scroll', () =>  {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0 )
 })
+
+
+//show/hide faqs answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus";
+
+        }
+    })
+})
